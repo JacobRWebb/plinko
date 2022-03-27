fd779720a9e7520d4fb708fa9aee068a6026e1e7
JacobRWebb <xxpwnage37xx@gmail.com>
2022-03-27 01:04:56 -0500

    Socket Status implemented

--- src/components/SocketStatus.tsx
+++ src/components/SocketStatus.tsx
@@ -1,10 +1,26 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
+import { globalContext } from "../globalContext";
+import { clsxm } from "../util/helpers";
 
 const SocketStatus: FunctionComponent = () => {
+  const context = useContext(globalContext);
+
   return (
     <span className="relative flex h-2 w-2 items-center justify-center">
-      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"></span>
-      <span className="relative inline-flex h-2 w-2 rounded-full bg-red-600"></span>
+      <span
+        className={clsxm(
+          "absolute inline-flex h-full w-full animate-ping rounded-full opacity-75",
+          context.socketConnected ? "bg-green-500" : "bg-red-500"
+        )}
+      ></span>
+      <span
+        className={clsxm(
+          "relative inline-flex h-2 w-2 rounded-full",
+          context.socketConnected ? "bg-green-500" : "bg-red-500"
+        )}
+      ></span>
     </span>
   );
 };
+
+export default SocketStatus;
--- src/components/navbar/UserNavSection.tsx
+++ src/components/navbar/UserNavSection.tsx
@@ -1,23 +1,14 @@
 import { FunctionComponent, useContext } from "react";
 import { globalContext } from "../../globalContext";
+import SocketStatus from "../SocketStatus";
 
 const UserNavSection: FunctionComponent = () => {
   const context = useContext(globalContext);
 
   return (
-    <div className="flex cursor-pointer select-none flex-row items-center justify-between space-x-2 rounded-sm bg-gray-200 px-2 py-1 shadow-sm shadow-gray-400 hover:shadow-md hover:shadow-gray-400">
-      <h1 className="text-lg font-medium text-gray-800">Username</h1>
-      {context.socketConnected ? (
-        <span className="relative flex h-2 w-2 items-center justify-center">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-500 opacity-75"></span>
-          <span className="relative inline-flex h-2 w-2 rounded-full bg-green-600"></span>
-        </span>
-      ) : (
-        <span className="relative flex h-3 w-3 items-center justify-center">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"></span>
-          <span className="relative inline-flex h-2 w-2 rounded-full bg-red-600"></span>
-        </span>
-      )}
+    <div className="flex cursor-pointer select-none flex-row items-center justify-between space-x-2 rounded-sm bg-gray-100 px-2 py-1 shadow-sm shadow-gray-200 hover:shadow-md hover:shadow-gray-300">
+      <h1 className="text-lg font-medium text-gray-800">Jacob Webb</h1>
+      <SocketStatus />
     </div>
   );
 };
